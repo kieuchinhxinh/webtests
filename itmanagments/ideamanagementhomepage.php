368ef22714e4660b6f727d48e82be6eda1d20890
Sangnguyen79776 <145439978+Sangnguyen79776@users.noreply.github.com>
2023-11-06 23:28:22 -0600

    Update ideamanagementhomepage.php

--- itmanagments/ideamanagementhomepage.php
+++ itmanagments/ideamanagementhomepage.php
@@ -11,7 +11,9 @@ session_start();
                 <a href="createidea.php">Post a new idea</a><br>
                <a href="viewallofidea.php">View list of idea</a><br>
            
-               <a href="staff.php">Go back to staff homepage</a>
+               <a href="staff.php">Go back to staff homepage</a><br>
+                <a href="postideawithpdffile.php">Post a new idea by pdf file</a>
+                                                            
             </div>
     </body>
 </html>
